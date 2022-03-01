4d629c9ad50b899124607e3f8a51d3057571d102
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-03-01 17:11:36 +0100

    ingredient and recipe-ingrediebt seed created

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipes_ingredients
+  has_many :recipes_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipes_ingredients
   validates :name, presence: true, uniqueness: true
   validates :instructions, presence: true, uniqueness: true
