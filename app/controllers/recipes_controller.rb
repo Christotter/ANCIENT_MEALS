98896a9ffdb9d2123ee0b07e1468a5270c6efb4c
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-03-02 16:44:47 +0100

    ingredient form version - in ingrediemt share page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,12 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:query]
-      @recipes = Recipe.joins(:user).where('instructions ILIKE :search_query OR country ILIKE :search_query OR recipes.name ILIKE :search_query', search_query: "%#{params[:query]}%")
+    if params[:ingredients]
+      @recipes = Recipe.joins(:recipes_ingredients).where("recipes_ingredients.ingredient_id IN (?)", params[:ingredients])
+      # TODO: find a better way of doing this (THIS IS HORRIBLE)
+      @recipes = @recipes.select { |recipe| @recipes.to_a.count(recipe) >= params[:ingredients].length }.uniq
+    elsif params[:query]
+      @recipes = Recipe.where('instructions ILIKE :search_query OR country ILIKE :search_query OR recipes.name ILIKE :search_query', search_query: "%#{params[:query]}%")
     else
       @recipes = Recipe.all
     end
