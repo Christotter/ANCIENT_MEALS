338b53c6b9e59699a2504cee399d04a30e819c37
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-03-07 16:52:50 +0100

    created remove add to favorites button

--- app/controllers/user_favorites_controller.rb
+++ app/controllers/user_favorites_controller.rb
@@ -1,6 +1,33 @@
 class UserFavoritesController < ApplicationController
   def index
     @user_favorites = UserFavorite.all
+  end
 
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @user_favorite = UserFavorite.new
+  end
+
+  # Needs to be check
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @user_favorite = UserFavorite.new
+    @user_favorite.recipe = @recipe
+
+    @user_favorite.user = current_user
+
+    if @user_favorite.save
+      redirect_to recipe_path(@recipe), notice: 'Added in favorites'
+    else
+      redirect_to recipe_path(@recipe), notice: "There was an error"
+    end
+  end
+  #---------------
+
+  def destroy
+    @user_favorite = UserFavorite.find(params[:id])
+    #@recipe = Recipe.find(params[:id])
+    @user_favorite.destroy
+    redirect_to recipe_path(@user_favorite.recipe),  notice: 'Removed from favorites'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :photo
+  has_many :user_favorites
 
   validates :email, presence: true
 end
